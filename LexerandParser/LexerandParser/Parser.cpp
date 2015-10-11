774090963046d160153deda539a2f440e1d74964
blghns <bilgehan.saglik@gmail.com>
2015-10-11 16:34:50 -0400

    Parser Files skeleton complete! Now DEBUG!

---
+++ LexerandParser/LexerandParser/Parser.cpp
@@ -0,0 +1,167 @@
+#include "Parser.h"
+
+// ***************************************************************
+// Main to test the parser
+// ***************************************************************
+int main(void) {
+	Parser parser; // create a parser object
+}
+
+// ***************************************************************
+// Constructor
+// ***************************************************************
+Parser::Parser(void)
+{
+	if (lexer.openFile()) {
+		lexer.getChar();
+		nextToken = lexer.lex();
+		cout << "First token is: " << nextToken << "\n";
+		essay();
+	}
+}
+
+
+Parser::~Parser(void)
+{
+}
+
+// ************************************************************************
+//  <essay> -> {<paragraph>} the_end.
+// ************************************************************************
+void Parser::essay(void){
+	cout << "essay" << endl;
+	do{
+		paragraph();
+	} while (lexer.lex() != "the_end");
+}
+
+// ************************************************************************
+//  <paragraph> -> for_starters {<sentence>.(<comment>)} concluded.
+// ************************************************************************
+void Parser::paragraph(void){
+	cout << "paragraph" << endl;
+	if (lexer.lex() == "for_starters"){
+		do{
+			sentence();
+			comment();
+		} while (lexer.lex() != "concluded.");
+	}
+}
+
+// ************************************************************************
+//  <sentence> -> <noun>(<verb> | <past_tense> | <future_tense>) [<numeric>] { (object | <subject> | <noun>)}
+// ************************************************************************
+
+void Parser::sentence(void){
+	cout << "sentence" << endl;
+	noun();
+	if (nextToken == "did")
+		past_tense();
+	else if (nextToken == "will")
+		future_tense();
+	else
+		verb();
+	do{
+		numeric();
+	} while (lexer.lex() != numeric);
+	do{
+		nextToken = lexer.lex();
+		if (isObject(nextToken))
+			object();
+		else if (isSubject(nextToken))
+			subject();
+		else
+			noun();
+	} while (lexer.lex != ".");
+}
+
+// ************************************************************************
+//  <comment> -> “(“ {a...z | 0...9} “)”
+// ************************************************************************
+// basically skips whatever is in the parenthesis.
+
+void Parser::comment(void){
+	cout << "comment" << endl;
+	if (lexer.lex == "("){
+		do{
+			nextToken = lexer.lex();
+		} while (lexer.lex != ")");
+	}
+	nextToken = lexer.lex();
+}
+
+// ************************************************************************
+//  <noun> -> (A...Z) {a...z}
+// ************************************************************************
+void Parser::noun(void){
+	if (nextToken == nouns){
+
+	}
+}
+
+// ************************************************************************
+//  <verb> -> have | is | add | divide | subtract | make | bake | print | fly | sleep | snooze
+// ************************************************************************
+void Parser::verb(void){
+	if (nextToken == have || nextToken == is || nextToken == add || nextToken == divide || nextToken == subtract || make || nextToken == bake || nextToken == print || nextToken == fly || nextToken == sleep || nextToken == snooze)
+		nextToken = lexer.lex();
+	else
+		cout << "Error reading verb" << endl;
+}
+
+// ************************************************************************
+//  <past_tense> -> did <verb>
+// ************************************************************************
+void Parser::past_tense(void){
+	if (nextToken == did){
+		nextToken = lexer.lex();
+		verb();
+	}
+	else
+		cout << "Error in past_tense, expecting did" << endl;
+}
+
+// ************************************************************************
+//  <future_tense> -> will <verb>
+// ************************************************************************
+void Parser::future_tense(void){
+	if (nextToken == will){
+		nextToken = lexer.lex();
+		verb();
+	}
+	else
+		cout << "Error in future tense, expecting will" << endl;
+}
+
+// ************************************************************************
+//  <subject> -> coffee | bed | ramen | pasta | cake | alarm | homework | program
+// ************************************************************************
+void Parser::subject(void){
+	if (nextToken == coffee || nextToken == bed || nextToken == ramen || nextToken == pasta || nextToken == cake || nextToken == alarm || nextToken == homework || nextToken == program)
+		nextToken = lexer.lex();
+	else
+		cout << "Error in subject" << endl;
+}
+
+// ************************************************************************
+//  <object> -> negative | plus | minus | and | or | multiply | divided_by
+// ************************************************************************
+void Parser::object(void){
+	if (nextToken == negative || nextToken == plus || nextToken == minus || nextToken == and || nextToken == or || nextToken == multiply || nextToken == divided_by)
+		nextToken = lexer.lex();
+	else
+		cout << "Error in object" << endl;
+}
+
+// ************************************************************************
+// <numeric> -> 0...9, { 0...9 }
+// ************************************************************************
+void Parser::numeric(void){
+	if (nextToken == number){
+		while (nextToken != number){
+			nextToken = lexer.lex();
+		}
+	}
+	else
+		cout << "Error in number" << endl;
+}
---
+++ LexerandParser/LexerandParser/Parser.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Lexer.h"
+
+class Parser
+{
+	Lexer lexer;
+	int nextToken;
+
+public:
+	Parser(void);
+	~Parser(void);
+
+	void essay(void);
+	void paragraph(void);
+	void sentence(void);
+	void comment(void);
+	void noun(void);
+
+	void verb(void);
+	void past_tense(void);
+	void future_tense(void);
+
+	void subject(void);
+	void object(void);
+	void numeric(void);
+
+};
+
+
